8e0e98f6b18df9cbe3a62289978acf782a087b81
Cory Coward <corydottech@gmail.com>
2026-09-16 21:22:50 -0400

    Make variables const

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,12 @@
 int main() {
     TcpServer server;
 
-    auto config = TcpServerConfig{
+    constexpr auto config = TcpServerConfig{
         .port = 8080,
         .max_backlog = 5
     };
     
-    auto result = server.start(config);
+    const auto result = server.start(config);
 
     std::cout << "Server start result: " << (result.is_success() ? "success" : "failure") << "\n";
     return 0;
