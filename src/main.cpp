434c07fad958d43a96dd8faf866941ef07588f4a
Cory Coward <ccoward80@gmail.com>
2026-09-19 23:27:13 -0400

    Improve workflow tests

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,20 @@
 #include <iostream>
 #include <string_view>
 
+namespace fs = std::filesystem;
+
+void test_start_twice();
+
+void test_accept_before_start();
+
+int test_full_lifecycle(fs::path config_path);
+
 int main(int argument_count, char *arguments[])
 {
-    std::filesystem::path config_path{HttpServerConfigLoader::DEFAULT_CONFIG_PATH};
+    test_start_twice();
+    test_accept_before_start();
+
+    fs::path config_path{HttpServerConfigLoader::DEFAULT_CONFIG_PATH};
 
     if (argument_count == 3 && std::string_view{arguments[1]} == "--config")
     {
@@ -19,6 +30,43 @@ int main(int argument_count, char *arguments[])
         return 1;
     }
 
+    return test_full_lifecycle(config_path);
+}
+
+void test_start_twice()
+{
+    TcpServer server;
+    const auto first_start_result = server.start({});
+
+    if (!first_start_result)
+    {
+        std::cerr << "Failed to start server the first time: " << first_start_result.error().socket_error.diagnostic << "\n";
+        return;
+    }
+
+    const auto second_start_result = server.start({});
+
+    if (!second_start_result)
+    {
+        std::cerr << "Failed to start server the second time: " << second_start_result.error().socket_error.diagnostic << "\n";
+    }
+
+    server.stop();
+}
+
+void test_accept_before_start()
+{
+    TcpServer server;
+    const auto accept_result = server.accept_connection();
+
+    if (!accept_result)
+    {
+        std::cerr << "Failed to accept connection before starting server: " << accept_result.error().socket_error.diagnostic << "\n";
+    }
+}
+
+int test_full_lifecycle(fs::path config_path)
+{
     HttpServerConfigLoader config_loader;
     const auto config_result = config_loader.load(config_path);
 
